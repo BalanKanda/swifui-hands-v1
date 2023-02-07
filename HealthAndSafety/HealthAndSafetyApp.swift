26c1a77e8b2deb4db4a290f0bfa95f404939ecaf
Balan Kandasamy <b_kwl@hotmail.co.uk>
2023-02-07 12:25:23 +0000

    Initial Commit

---
+++ HealthAndSafety/HealthAndSafetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAndSafetyApp.swift
+//  HealthAndSafety
+//
+//  Created by Balan Kandasamy on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthAndSafetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
