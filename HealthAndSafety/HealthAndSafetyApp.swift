11468566d6f57995da45802c6e597002c992ebd8
Balan Kandasamy <b_kwl@hotmail.co.uk>
2023-02-07 16:01:46 +0000

    - organise files into folders - add content model - set content model as environment object

--- HealthAndSafety/HealthAndSafetyApp.swift
+++ HealthAndSafety/HealthAndSafetyApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct HealthAndSafetyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ HealthAndSafety/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  HealthAndSafety
+//
+//  Created by Balan Kandasamy on 07/02/2023.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
